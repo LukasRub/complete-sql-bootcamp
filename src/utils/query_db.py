64c3cbc7b9433b2b8fb0873753bb5e3bb74eeb3e
LukasRub <lukas.rubikas@gmail.com>
2023-03-12 13:20:10 +0200

    Simplified db connection utils with partials

--- src/utils/query_db.py
+++ src/utils/query_db.py
@@ -1,8 +1,11 @@
 from typing import Generator, Optional
+from functools import partial
 from contextlib import contextmanager
 
-import psycopg2 as pg2
 import pandas as pd
+import psycopg2 as pg2
+from dotenv import dotenv_values
+from IPython.display import display
 
 
 class Connection:
@@ -10,7 +13,7 @@ class Connection:
     def __init__(self, connection) -> None:
         self.connection = connection
 
-    def fetch_all(self, query: str) -> Optional[tuple[pd.DataFrame, int]]:
+    def fetch_all(self, query: str) -> Optional[pd.DataFrame]:
         with self.connection.cursor() as cursor:
             try:
                 cursor.execute(query)
@@ -19,12 +22,8 @@ class Connection:
             else:
                 columns = [column.name for column in cursor.description]
                 results = cursor.fetchall()
-                row_count = cursor.rowcount
 
-                return (pd.DataFrame(results, columns=columns), row_count)
-            
-
-
+                return display(pd.DataFrame(results, columns=columns))
 
 
 @contextmanager
@@ -36,3 +35,7 @@ def postgres_connect(**kwargs) -> Generator:
         print(e)
     finally:
         connection.close()
+
+
+conn_options = {key.lower():value for key, value in dotenv_values().items()}
+db_connection = partial(postgres_connect, **conn_options)
